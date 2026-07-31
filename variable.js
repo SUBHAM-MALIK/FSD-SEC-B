4571b40fe2f726cc853aba71b9da3eb06870d4bb
Subham Malik <subhammalik382@gmail.com>
2026-07-31 14:49:06 +0530

    variable_and_function

---
+++ variable.js
@@ -0,0 +1,14 @@
+console.log(a);
+a=31;
+var a;
+a=65;
+  function f(){
+    console.log(a);
+    var b=81;
+    var a =21;
+    console.log(a);
+    console.log(b);
+    b=67;
+}
+console.log(f);
+f();
